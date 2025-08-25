69eabd50dfe3c7ea87ed1bcccdeaf7b0312e7111
Likhith-23 <likhith.nenavath@gmail.com>
2025-08-25 16:28:15 +0530

    ai-travel-planner

--- ai-travel-planner/backend/routes/weather.py
+++
@@ -1,9 +0,0 @@
-# backend/routes/weather.py
-from fastapi import APIRouter
-from utils.openweather import get_weather
-
-router = APIRouter()
-
-@router.get("/weather")
-def get_city_weather(city: str):
-    return get_weather(city)
